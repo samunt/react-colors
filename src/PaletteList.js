08be6c23ecc7c7f9965d666b9454976e3601f31f
samunt <sammy.unterman@gmail.com>
2020-07-02 13:34:55 -0400

    material ui WithStyles and higher order component example and functional component example

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import MiniPalette from './MiniPalette';
 import {Link} from 'react-router-dom';
 class PaletteList extends Component {
     render() {
         const {palette} = this.props;
         return (
             <div>
+                <MiniPalette/>
                 {palette.map(palette => (
                     <p>
                         <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
